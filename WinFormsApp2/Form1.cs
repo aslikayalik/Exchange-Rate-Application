3149ec57bf312996d404b3c0ce1bda10dcb803e5
ASLI KAYALIK <96055823+aslikayalik@users.noreply.github.com>
2024-01-16 19:23:42 +0300

    Update Form1.cs

--- WinFormsApp2/Form1.cs
+++ WinFormsApp2/Form1.cs
@@ -20,29 +20,21 @@ namespace WinFormsApp
 
         private void btn_Jsonkurgu_Click(object sender, EventArgs e)
         {
-           // Core.BusinessLogicLayer BLL = new Core.BusinessLogicLayer();
-           // BLL.KurBilgileriniGuncelle();
+          
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            // Formumuz load olduğunda ilk olarak ne yapmalıyız :
-            // core ın içerisindeki BusinessLogicLayer ı yine BLL olarak örnekleyelim ve para birimlerini
-            // çekelim.
-            Core.BusinessLogicLayer BLL = new Core.BusinessLogicLayer(); // önce businessLogicLayer a bir instance oluşturdum.
-            BLL.KurBilgileriniGuncelle(); // KurGeçmiş tarafına benim bir önceki değerlerimin kayıtlı olması gerekiyor.
-            List<Entities.ParaBirimi> ParaBirimleri = BLL.ParaBirimiListesi(); // ParaBirimlerini bir list genreic üstüne aldım.
-            List<Entities.Kur> KurBilgileri = BLL.KurListe(); // o an geçerli olan kurlarımın listesini çektim.
-             // KurBilgileri içerisinde paraBirim id si var, id yi bilemeyeceğime göre çünkü guid, paraBiriminin koduna göre 
-             // select atmam gerekiyor.
+        
+            Core.BusinessLogicLayer BLL = new Core.BusinessLogicLayer();
+            BLL.KurBilgileriniGuncelle();
+            List<Entities.ParaBirimi> ParaBirimleri = BLL.ParaBirimiListesi(); 
+            List<Entities.Kur> KurBilgileri = BLL.KurListe(); .
+         
             Entities.Kur Dolar = KurBilgileri.FirstOrDefault(I => I.ParaBirimiID == ParaBirimleri.FirstOrDefault(x => x.Code == "USD").ID);
-            // Her bir kaydı I olarak isimlendiriyorum, her bir kaydın içeriisndeki ParaBirimiID eşit ise ParaBirimleri
-            // listemin içeriisnde yine bir arama yapıyorum FirstOrDefault ile, Parabirimleri içerisinde code u USD olan datanın 
-            // ID sine eşit ise Dolar ın tiplerinin verileri bu Dolar içinde duracak.
-            lbl_dolar_alis.Text = Dolar.Alis.ToString(); // Dolar değerini decimal ın N2 formatına göre ekrana yazdırdım.
-            lbl_dolar_satis.Text = Dolar.Satis.ToString(); // Burada parantez içinde N2 vardı onları sildik yuvarlamaya sebep
-            // oluyor diye.
-
+    
+            lbl_dolar_alis.Text = Dolar.Alis.ToString(); 
+            lbl_dolar_satis.Text = Dolar.Satis.ToString(); 
 
             Entities.Kur Euro = KurBilgileri.FirstOrDefault(I => I.ParaBirimiID == ParaBirimleri.FirstOrDefault(x => x.Code == "EUR").ID);
             lbl_euro_alis.Text = Euro.Alis.ToString();
